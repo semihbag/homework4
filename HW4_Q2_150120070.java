a9bdb81e237ebc80a580007e76f5d0f8524a9674
semihbag <105243010+semihbag@users.noreply.github.com>
2022-10-05 23:42:13 +0300

    Update HW4_Q2_150120070.java

--- HW4_Q2_150120070.java
+++ HW4_Q2_150120070.java
@@ -1,6 +1,6 @@
 // Name: Semih
-// Surname : Bað
-// Student ID: 150120070
+// Surname : BaÄŸ
+
 
 /* Purpose : Write a program that will determine whether or not it is valid per the Luhn formula.
  *  The Luhn algorithm is a simple checksum formula used to validate a variety of identification 
